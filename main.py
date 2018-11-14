40be53092dc55f583be3a8bd0fa0532d11af368d
Mahdi Enan <mahdi.cie@gmail.com>
2018-11-14 19:38:26 +0100

    added nac skeleton with init values

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from agents.npgagent import NPGAgent
+from agents.nacagent import NACAgent
 from npg.npg import NPG
+from nac.nac import NAC
 from npg.models.linear import Linear
 
 import gym
@@ -9,7 +11,13 @@ from tqdm import tqdm
 
 env = gym.make('CartpoleStabShort-v0')
 
-policy = NPG(env.observation_space.shape, env.action_space.shape, Linear)
-agent = NPGAgent(policy, env)
+#policy = NPG(env.observation_space.shape, env.action_space.shape, Linear)
+#agent = NPGAgent(policy, env)
 
-agent.train_episode(3)
+#agent.train_episode(3)
+
+
+model = NAC(env.observation_space.shape, env.action_space.shape, Linear)
+agent = NACAgent(model, env)
+
+agent.train()
